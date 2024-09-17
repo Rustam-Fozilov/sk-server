f4068212b81af79346678b31cd6abf6e00902bed
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-09-17 19:55:51 +0500

    add: helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function() {
+            Route::middleware('api' . getLang())
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
@@ -17,3 +22,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
+function getLang() {
+    $url = explode('/', request()->url());
+    $url = array_slice($url, 3);
+
+    $lang = '';
+    $p = @$url[0] == 'api' ? 1 : 0;
+    if (in_array(@$url[$p], config('app.languages'))) {
+        $lang = '/' . $url[$p];
+        app()->setLocale($url[$p]);
+    }
+    return $lang;
+}
