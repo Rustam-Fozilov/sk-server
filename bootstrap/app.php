ea4e53c4e6de3e3f982777e6a2b5dd1077e9b172
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-12-16 13:19:47 +0500

    refactor routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -7,10 +8,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using: function () {
+            $lang = getLang();
+
+            Route::middleware('api')
+                ->prefix('api' . $lang)
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->prefix($lang)
+                ->group(base_path('routes/web.php'));
+        },
     )
     ->withSchedule(function(Schedule $schedule) {
         $schedule->command('telescope:prune')->everyMinute();
