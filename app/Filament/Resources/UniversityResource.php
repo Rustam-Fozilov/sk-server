342ebaecd9e1088028848c119c86fac3b4891ac6
Rustam Fozilov <thisisrustam@gmail.com>
2024-12-20 22:50:04 +0500

    university create almost done

---
+++ app/Filament/Resources/UniversityResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UniversityResource\Pages;
+use App\Models\University;
+use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class UniversityResource extends Resource
+{
+    protected static ?string $label = "Universitet";
+
+    protected static ?string $pluralLabel = "Universitetlar";
+
+    protected static ?string $model = University::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()
+                ->schema([
+                    Section::make([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Name')
+                            ->required(),
+                        Forms\Components\TextInput::make('address')
+                            ->label('Address')
+                            ->required(),
+                        Forms\Components\TextInput::make('website')
+                            ->label('Website')
+                            ->required(),
+                    ]),
+                ]),
+                Group::make()
+                ->schema([
+                    Section::make([
+                        Forms\Components\MarkdownEditor::make('info')
+                            ->label('Info')
+                            ->required(),
+                        Forms\Components\FileUpload::make('image_link')
+                            ->image()
+                            ->deletable(true)
+                            ->fetchFileInformation(true)
+                            ->openable()
+                            ->disk('public')
+                            ->directory('universities')
+                            ->label('Image')
+                            ->required(),
+                    ])
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('address')->limit(20),
+                Tables\Columns\TextColumn::make('website')->limit(20),
+                Tables\Columns\TextColumn::make('info')->limit(20),
+                Tables\Columns\ImageColumn::make('image_link')->label('image')->limit(20),
+                Tables\Columns\TextColumn::make('created_at'),
+                ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUniversities::route('/'),
+            'create' => Pages\CreateUniversity::route('/create'),
+            'edit' => Pages\EditUniversity::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/UniversityResource/Pages/EditUniversity.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\UniversityResource\Pages;
+
+use App\Filament\Resources\UniversityResource;
+use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUniversity extends EditRecord
+{
+    protected static string $resource = UniversityResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    
+}
