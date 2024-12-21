cbddcd3c41f9ea25562d4a5f32069e24d117661e
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-12-21 13:35:21 +0500

    user CRUD done

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class UserResource extends Resource
+{
+    protected static ?string $label = "Foydalanuvchi";
+
+    protected static ?string $pluralLabel = "Foydalanuvchilar";
+
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make([
+                        Forms\Components\TextInput::make('name')->required(),
+                        Forms\Components\TextInput::make('surname'),
+                        Forms\Components\TextInput::make('phone')->required(),
+                    ]),
+                ]),
+                Group::make()->schema([
+                    Section::make([
+                        Forms\Components\TextInput::make('chat_id'),
+                        Forms\Components\TextInput::make('password')->password(),
+                        Forms\Components\Checkbox::make('is_admin')->default(false),
+                    ]),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('ID')->sortable(),
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('surname')->searchable(),
+                Tables\Columns\TextColumn::make('phone')->searchable(),
+                Tables\Columns\TextColumn::make('chat_id')->searchable(),
+                Tables\Columns\BooleanColumn::make('is_admin')->label('Is Admin'),
+                Tables\Columns\TextColumn::make('created_at'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
