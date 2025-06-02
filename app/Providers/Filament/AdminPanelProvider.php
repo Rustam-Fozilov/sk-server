a18cc50a35cc1707ef577d3e7725b27c335c3feb
Rustam Fozilov <thisisrustam@gmail.com>
2025-06-02 10:50:37 +0500

    add profile to filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(Login::class)
             ->spa()
+            ->profile()
             ->colors([
                 'primary' => Color::Amber,
             ])
